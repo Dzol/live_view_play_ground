050fb0beed6d856fe82e9a45fd7e2717e8cb018b
Dzol <joseph.yiasemides@port-zero.com>
2021-08-13 15:04:53 +0200

    Fix spelling in temperature unit C

--- lib/live_view_play_ground_web/live/temperature_converter_live.ex
+++ lib/live_view_play_ground_web/live/temperature_converter_live.ex
@@ -5,7 +5,7 @@ defmodule LiveViewPlayGroundWeb.TemperatureConverterLive do
   def render(assigns) do
     ~L"""
     <h1>Temperature Converter</h1>
-    <input phx-keyup="celcius-change" type="text" value="<%= @celcius %>" style="width: 8em">
+    <input phx-keyup="celsius-change" type="text" value="<%= @celsius %>" style="width: 8em">
     &nbsp℃ =&nbsp
     <input phx-keyup="farenheit-change" type="text" value="<%= @farenheit %>" style="width: 8em">
     &nbsp℉
@@ -13,13 +13,13 @@ defmodule LiveViewPlayGroundWeb.TemperatureConverterLive do
   end
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, %{celcius: nil, farenheit: nil})}
+    {:ok, assign(socket, %{celsius: nil, farenheit: nil})}
   end
 
-  def handle_event("celcius-change", %{"value" => c}, socket) do
+  def handle_event("celsius-change", %{"value" => c}, socket) do
     socket = case Float.parse(c) do
       {c, ""} ->
-        assign(socket, %{farenheit: farenheit(c), celcius: Kernel.round(c)})
+        assign(socket, %{farenheit: farenheit(c), celsius: Kernel.round(c)})
       _ ->
         socket
     end
@@ -29,14 +29,14 @@ defmodule LiveViewPlayGroundWeb.TemperatureConverterLive do
   def handle_event("farenheit-change", %{"value" => f}, socket) do
     socket = case Float.parse(f) do
       {f, ""} ->
-        assign(socket, %{farenheit: Kernel.round(f), celcius: celcius(f)})
+        assign(socket, %{farenheit: Kernel.round(f), celsius: celsius(f)})
       _ ->
         socket
     end
     {:noreply, socket}
   end
 
-  defp celcius(f) do
+  defp celsius(f) do
     Kernel.round((f - 32) * (5 / 9))
   end
 
